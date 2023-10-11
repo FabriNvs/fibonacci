350e19cd5fd44ac75d33ac61225a0032767e03a2
diarash <dianasuchiapa@gmai.com>
2023-10-11 12:36:50 -0600

    fibo sequence

---
+++ fibo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#By FabriNvs
+#GNU/GPL License
+#My first secuence
+
+#sumar los primeros 10 números enteros
+
+N=100
+a=1
+b=1
+print(a)
+print(b)
+#c=a+b
+
+suma=0
+for x in range(N+1):
+    c=a+b
+    print(c)
+    b1=b
+    a=b1
+    b=c
+    
+    
+    
